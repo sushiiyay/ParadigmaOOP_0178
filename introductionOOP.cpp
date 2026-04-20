705a149819c97d056c6856e7e4addff01363c967
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-20 14:23:03 +0700

    Batas class

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -9,6 +9,7 @@ public :
 
     void printData(){
         cout << "NIM : " << nim << endl;
-
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
     }
-}
+}; //batas class
